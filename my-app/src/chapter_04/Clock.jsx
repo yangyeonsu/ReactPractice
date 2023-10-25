c3ca71f476986ee7efae3f0c91cf977307fcfa96
yangyeonsu <yeonsu9910@gmail.com>
2023-10-25 11:57:24 +0900

    upload[chapter_04]

---
+++ my-app/src/chapter_04/Clock.jsx
@@ -0,0 +1,13 @@
+// 현재 시간을 출력하는 컴포넌트
+import React from "react";
+
+function Clock(props) {
+    return (
+        <div>
+            <h1>안녕, 리액트!</h1>
+            <h2>현재 시간 : {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+}
+
+export default Clock;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter_03/Library';
+import Library from './chapter_03/Library'; // chapter_03
+import Clock from './chapter_04/Clock';     // chapter_04
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+// 추가
+const root = createRoot(document.getElementById("root"));
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
